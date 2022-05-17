2a467345b88ff0278e7bd726189a435b7e66953f
Sukanya Ghosh <sukanyaghosh807@gmail.com>
2022-05-17 10:34:15 -0400

    new branch

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -114,7 +114,7 @@ L.control.layers(baseMaps).addTo(map);
 
 
 // Accessing the airport GeoJSON URL
-let airportData="https://raw.githubusercontent.com/Sukanya807/Mapping_Earthquakes/Mapping_GeoJSON_Points/majorAirports.json";
+let airportData="https://raw.githubusercontent.com/Sukanya807/Mapping_Earthquakes/main/majorAirports.json";
 
 // Grabbing our GeoJSON data.
 d3.json(airportData).then(function(data){
